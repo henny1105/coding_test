80e47288c11bc692c567dcf887c1f53ef1161e18
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-06-17 22:27:28 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const num = Number(input[0]);
+let sum = 0;
+for (let i = 1; i <= num; i++) {
+	sum += i;
+}
+console.log(sum);
