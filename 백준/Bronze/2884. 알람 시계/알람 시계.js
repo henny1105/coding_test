b9cd7cce18571db686804e8bae3ca6046c644f73
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-06-17 19:19:27 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -2,10 +2,8 @@ const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
 const a = Number(input[0]);
 const b = Number(input[1]);
 
-if (0 <= a <= 23 && 0 <= b <= 59) {
-	if (b < 45) {
-		console.log(a === 0 ? 23 : a - 1, b + 15);
-	} else {
-		console.log(a, b - 45);
-	}
+if (b < 45) {
+	console.log(a === 0 ? 23 : a - 1, b + 15);
+} else {
+	console.log(a, b - 45);
 }
