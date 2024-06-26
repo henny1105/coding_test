d558640ab5f10d4605a4adad490c007c64024da3
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-06-26 11:42:07 +0900

    [Bronze V] Title: A+B - 8, Time: 96 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const count = parseInt(input[0]);
+
+let result = '';
+
+for (let i = 1; i <= count; i++) {
+	let array = input[i].split(' ').map(Number);
+	let sum = array[0] + array[1];
+
+	result += `Case #${i}: ${array[0]} + ${array[1]} = ${sum}\n`;
+}
+
+console.log(result);
