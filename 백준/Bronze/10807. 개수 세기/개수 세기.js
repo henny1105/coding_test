5f38ee1241385fe3eeab4cf7ba222fde05b3cb3b
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-07-01 16:56:04 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -1,14 +1,12 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = Number(input[0]);
-const numbers = input[1].split(' ').map(Number);
-const v = Number(input[2]);
+const num = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const searchNum = Number(input[2]);
+let result = 0;
 
-let count = 0;
-
-for (let i = 0; i < N; i++) {
-	if (numbers[i] === v) {
-		count++;
+for (let i = 0; i <= num; i++) {
+	if (arr[i] === searchNum) {
+		result += 1;
 	}
 }
-
-console.log(count);
+console.log(result);
