b4f7a28d35817af0d6be37f97b2d43e552fa6ece
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-07-30 14:51:03 +0900

    [Bronze III] Title: 최소, 최대, Time: 448 ms, Memory: 92280 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,11 +1,4 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = parseInt(input[0]);
-const numbers = input[1].split(' ').map(Number);
-let result = '';
+const arr = input[1].split(' ').map(Number);
 
-const min = Math.min(...numbers);
-const max = Math.max(...numbers);
-
-result += `${min} ${max}`;
-
-console.log(result);
+console.log(Math.min(...arr), Math.max(...arr));
