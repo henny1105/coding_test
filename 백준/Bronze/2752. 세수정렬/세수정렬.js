de71e00a253079688e68bbf9f1fcae9aa524e3ec
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-08-06 14:33:16 +0900

    [Bronze IV] Title: 세수정렬, Time: 96 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.js
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -1,4 +1,5 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-let arr = input.map(Number).sort((a, b) => a - b);
+let arr = input.map(Number);
+let sort = arr.sort((a, b) => a - b);
 
-console.log(arr.join(' '));
+console.log(sort.join(' '));
