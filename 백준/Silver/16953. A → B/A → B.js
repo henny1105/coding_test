f34c593fd700ee82c8aeadbea204eb29e5ba820e
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-08-22 21:40:23 +0900

    [Silver II] Title: A → B, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,26 +1,21 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-const [A, B] = input[0].split(' ').map(Number);
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+let [A, B] = input.split(' ').map(Number);
 
-function minOperations(A, B) {
-	let count = 1;
+let count = 1;
 
-	while (B !== A) {
-		if (B < A) {
-			return -1;
-		}
-
-		if (B % 2 === 0) {
-			B /= 2;
-		} else if (B % 10 === 1) {
-			B = Math.floor(B / 10);
-		} else {
-			return -1;
-		}
-
-		count++;
+while (B > A) {
+	if (B % 2 === 0) {
+		B = Math.floor(B / 2);
+	} else if (B % 10 === 1) {
+		B = Math.floor(B / 10);
+	} else {
+		break;
 	}
-
-	return count;
+	count++;
 }
 
-console.log(minOperations(A, B));
+if (B === A) {
+	console.log(count);
+} else {
+	console.log(-1);
+}
