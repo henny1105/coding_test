4cd20b9b1527ccaaaa8ffe7393d7ba0c16b2b2b2
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-08-22 21:23:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,21 +1,18 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 let num = Number(input[0]);
+let result = 0;
 
-function sugarDelivery(N) {
-	let count = 0;
-
-	while (N >= 0) {
-		if (N % 5 === 0) {
-			count += N / 5;
-			console.log(count);
-			return;
-		}
-		N -= 3;
-		count += 1;
+while (num >= 0) {
+	if (num % 5 === 0) {
+		result += num / 5;
+		console.log(result);
+		return;
 	}
-
-	console.log(-1);
+	num -= 3;
+	result += 1;
 }
 
-sugarDelivery(num);
+if (num < 0) {
+	console.log(-1);
+}
