b9b8f387b01db14000356111b3c773e8c0df82d4
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-08-30 11:40:54 +0900

    [Bronze V] Title: 오늘 날짜, Time: 88 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = (today.getMonth() + 1).toString().padStart(2, '0');
+const day = today.getDate().toString().padStart(2, '0');
+
+console.log(year + '-' + month + '-' + day);
