4f4d05a80387f5caee07ed9fb980017159ecfbd2
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-09-06 15:01:51 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 88 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let result = [];
+
+for (let i = 0; i < input.length - 1; i++) {
+	let [a, b] = input[i].split(' ').map(Number);
+	result.push(a + b);
+}
+
+console.log(result.join('\n'));
