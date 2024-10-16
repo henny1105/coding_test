b2c94952ca6d0959868c9ce14df84053011f97ae
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-10-16 15:15:38 +0900

    [Bronze II] Title: 거스름돈, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.js
+++ 백준/Bronze/5585. 거스름돈/거스름돈.js
@@ -1,14 +1,15 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
 
-let price = Number(input);
+const coin = Number(input[0]);
+const coinList = [500, 100, 50, 10, 5, 1];
+let change = 1000 - coin;
 
-let change = 1000 - price;
-let coins = [500, 100, 50, 10, 5, 1];
 let count = 0;
-
-for (let i = 0; i < coins.length; i++) {
-	count += Math.floor(change / coins[i]);
-	change %= coins[i];
+for (let i = 0; i < coinList.length; i++) {
+	count += Math.floor(change / coinList[i]);
+	change %= coinList[i];
 }
 
 console.log(count);
