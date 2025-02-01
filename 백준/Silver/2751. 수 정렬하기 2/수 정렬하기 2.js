a823a591d16c4c74f525a4c67c0776ab50e30978
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-02-02 01:16:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1184 ms, Memory: 216428 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
 
-let arr = input.slice(1);
-arr.sort((a, b) => a - b);
-
+input.shift();
+let arr = input.sort((a, b) => a - b);
 console.log(arr.join('\n'));
