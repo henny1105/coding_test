556b2bd7ea42018a22b4490e87d6f825442c3a4c
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-02-23 21:21:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,13 +1,7 @@
 function solution(numbers) {
-    let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let sum = 0;
+    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     
-    for(let i = 0; i < nums.length; i++) {
-        if(!numbers.includes(nums[i])) {
-            sum += nums[i];
-        }
-    }
+    let result = arr.reduce((acc,cur)=>acc+cur,0) - numbers.reduce((acc,cur)=>acc+cur,0);
     
-    return sum;
-    
+    return result;
 }
