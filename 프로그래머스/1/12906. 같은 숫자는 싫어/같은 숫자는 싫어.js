326f5181c686e44ed19fa9cc2e57c1ca7bfa400e
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-04-17 16:54:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.10 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr){
+    let result = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] !== result[result.length - 1]) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
