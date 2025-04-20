748d024c186277cea08ff0ac1af2910b36b9c568
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-04-20 15:24:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.45 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,16 +1,15 @@
 function solution(participant, completion) {
-    let result = '';
+    const map = new Map();
     
-    participant.sort();
-    completion.sort();
-    
-    for(let i = 0; i < participant.length; i++) {
-        if(participant[i] !== completion[i]) {
-            result = participant[i];
-            
-            break;
-        }
+    for(const name of participant) {
+        map.set(name, (map.get(name) || 0) + 1);
     }
     
-    return result;
+    for(const name of completion) {
+        map.set(name, map.get(name) - 1);
+    }
+    
+    for (const [name, count] of map.entries()) {
+      if (count > 0) return name;
+    }
 }
