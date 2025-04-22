82e9f1da21a85b1da4c33e649bf291ae49de4ae7
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-04-22 23:35:36 +0900

    [Bronze IV] Title: 영수증, Time: 92 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.js
+++ 백준/Bronze/25304. 영수증/영수증.js
@@ -1,17 +1,19 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const total = Number(input[0]);
-const num = Number(input[1]);
-let totalPrice = 0;
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
+let input = fs
+	.readFileSync(filePath)
+	.toString()
+	.trim()
+	.split('\n')
+	.map((line) => line.split(' ').map(Number));
 
-for (let i = 2; i <= num + 1; i++) {
-	const pair = input[i].split(' ').map(Number);
-	const product = pair[0] * pair[1];
+let [price] = input[0];
+let [n] = input[1];
+let result = 0;
 
-	totalPrice += product;
+for (let i = 2; i <= n + 1; i++) {
+	let [totalPrice, count] = input[i];
+	result += totalPrice * count;
 }
 
-if (totalPrice === total) {
-	console.log('Yes');
-} else {
-	console.log('No');
-}
+price === result ? console.log('Yes') : console.log('No');
